f1bb2a7625acd7a1d0b786ac4157c39528bd6b46
DiViNiZe <patric_emmel@outlook.com>
2017-11-25 23:08:19 +0700

    $25112017

--- routes/products.js
+++ routes/products.js
@@ -2,14 +2,14 @@ var express = require('express');
 var router = express.Router();
 var con = require('../models');
 var pool  = require('../models');
+var query = require('../models/query')
 
-
-/* GET users listing. */
 router.get('/',function(req, res, next) {
-  pool.query('select * from users',function(err,result,fields){
-    if(err)throw err;
-    res.json(JSON.stringify(result));
-  });
+  // pool.query(query.all,function(err,result,fields){
+  //   if(err)throw err;
+  //   res.json(JSON.stringify(result));
+  // });
+  query.test('Hi')
 });
 
 module.exports = router;
